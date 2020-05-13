eabfdb5ab5cc62773fb0af9cb73a0e2236bef719
ividanec <ividanec@gmail.com>
2020-05-13 20:51:49 +0200

    Popravljeno mjesto gdje se nalazi file,veze izmedu tablica nisu dobre

--- galerijaslika.sql
+++ galerijaslika.sql
@@ -1,4 +1,4 @@
-# c:\xampp\mysql\bin\mysql -uedunova -pedunova < c:\jp22\galerijaslika.sql
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova < c:\jp22\hello\galerijaslika.sql
 
 drop database if exists galerijaslika;
 create database galerijaslika;
@@ -7,14 +7,15 @@ use galerijaslika;
 create table autor(
     sifra int not null primary key auto_increment,
     ime varchar(50),
-    prezime varchar(50)
+    prezime varchar(50),
+    slika int not null
 );
 
 create table slika(
     sifra int not null primary key auto_increment,
     naziv varchar(50),
-    opis varchar(50),
-    autor int not null
+    opis varchar(50)
+
 );
 create table galerija(
     sifra int not null primary key auto_increment,
