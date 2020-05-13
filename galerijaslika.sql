094653f55ed86c830e449f17654e2812fa779df6
ividanec <ividanec@gmail.com>
2020-05-13 21:20:48 +0200

    Veze ispravljene

--- galerijaslika.sql
+++ galerijaslika.sql
@@ -4,26 +4,28 @@ drop database if exists galerijaslika;
 create database galerijaslika;
 use galerijaslika;
 
-create table autor(
+create table galerija(
     sifra int not null primary key auto_increment,
-    ime varchar(50),
-    prezime varchar(50),
-    slika int not null
+    naziv varchar(50),
+    opis varchar(50)
 );
 
 create table slika(
     sifra int not null primary key auto_increment,
     naziv varchar(50),
-    opis varchar(50)
-
-);
-create table galerija(
-    sifra int not null primary key auto_increment,
-    naziv varchar(50),
     opis varchar(50),
-    slika int not null
+    galerija int not null,
+    autor int not null
 );
 
-alter table autor add foreign key (slika) references slika(sifra);
+create table autor(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
+);
 
-alter table galerija add foreign key (slika) references slika(sifra);
+
+
+alter table slika add foreign key (galerija) references galerija(sifra);
+
+alter table slika add foreign key (autor) references autor(sifra);
