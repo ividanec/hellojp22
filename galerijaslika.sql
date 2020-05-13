1026b9a214bf3b408798268acdc67440b9615878
ividanec <ividanec@gmail.com>
2020-05-13 20:41:08 +0200

    Nedovrseno-ima neka greska

---
+++ galerijaslika.sql
@@ -0,0 +1,28 @@
+# c:\xampp\mysql\bin\mysql -uedunova -pedunova < c:\jp22\galerijaslika.sql
+
+drop database if exists galerijaslika;
+create database galerijaslika;
+use galerijaslika;
+
+create table autor(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
+);
+
+create table slika(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    opis varchar(50),
+    autor int not null
+);
+create table galerija(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50),
+    opis varchar(50),
+    slika int not null
+);
+
+alter table autor add foreign key (slika) references slika(sifra);
+
+alter table galerija add foreign key (slika) references slika(sifra);
